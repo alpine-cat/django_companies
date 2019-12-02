c8c3758bc37e228742d15d6c9b91d09c1988acbe
Trunova Daria <trunova@gmail.com>
2019-12-02 19:15:39 +0200

    WorkTime model added

--- companies/admin.py
+++ companies/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
-from .models import Company, Manager, Worker, Work, WorkPlace
+from .models import Company, Manager, Worker, Work, WorkPlace, WorkTime
 
 admin.site.register(Company)
 admin.site.register(Manager)
 admin.site.register(Work)
 admin.site.register(WorkPlace)
 admin.site.register(Worker)
+admin.site.register(WorkTime)
